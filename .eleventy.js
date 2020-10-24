1e4a3a7d5d4d5389c0f34f5fa0c4aaf4cea576b1
Chandan <chandanhem@gmail.com>
2020-10-24 14:46:51 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -17,23 +17,3 @@ module.exports = function(eleventyConfig) {
     return util.inspect(obj);
   });
 };
-
-const util = require('util');
-
-module.exports = function(eleventyConfig) {
-  // Output directory: _site
-
-  // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("img");
-
-  // Copy `css/fonts/` to `_site/css/fonts`
-  // If you use a subdirectory, it’ll copy using the same directory structure.
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("vendor");
-  eleventyConfig.addPassthroughCopy("admin");
-
-  eleventyConfig.addFilter('dump', obj => {
-    return util.inspect(obj);
-  });
-};
